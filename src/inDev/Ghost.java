cebd6748044733d01e9078d1137d9c30a5ec9e1b
Meftahi12 <abdomeftahi7@gmail.com>
2019-08-29 14:55:07 -0400

    hello

---
+++ src/inDev/Ghost.java
@@ -0,0 +1,27 @@
+package inDev;
+
+public class Ghost implements Character{
+	
+	public static boolean state = true;
+	private String color;
+	private GhostStrategy stategy;
+	
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void treatcollision() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
