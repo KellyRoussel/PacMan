21019e4528853d0e34dd186cdc535b8ecfb03332
Meftahi12 <abdomeftahi7@gmail.com>
2019-08-30 18:04:43 -0400

    lastOne

--- src/inDev/StatusBar.java
+++ src/inDev/StatusBar.java
@@ -1,9 +1,26 @@
 package inDev;
 
-public class StatusBar {
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class StatusBar extends JPanel{
+
 	
 	private int score = 0;
 	private int lives = 0;
+	
+	public StatusBar() {
+		setPreferredSize(new Dimension(600, 150));
+		
+		setLayout(new BorderLayout());
+		
+		add(new JLabel("score"), BorderLayout.CENTER);
+		add(new JLabel("vies"), BorderLayout.LINE_START);
+		add(new JLabel("niveau"), BorderLayout.LINE_END);
+	}
 	
 	public void load() {
 		
