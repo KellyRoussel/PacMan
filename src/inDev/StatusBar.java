9a6b528b3ef5b2363bf438ac05618ee0f3c47608
Meftahi12 <abdomeftahi7@gmail.com>
2019-08-31 16:51:11 -0400

    Lives

--- src/inDev/StatusBar.java
+++ src/inDev/StatusBar.java
@@ -5,7 +5,10 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.Image;
+import java.io.File;
 
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -13,26 +16,53 @@ public class StatusBar extends JPanel{
 
 	
 	private int score = 0;
-	private int lives = 0;
+	private int lives = 3;
+	
 	private JLabel scoreLabel = null;
-	private JLabel livesLabel = null;
-	private JLabel fruitsLabel = null;
+	private JPanel fruitsPane = null; 
+	
 	
 	public StatusBar() {
 		setBackground(Color.black);
 		setPreferredSize(new Dimension(600, 150));
 		setLayout(new GridLayout(1, 3));
-		scoreLabel = new JLabel();
 		
-		scoreLabel.setFont(new Font("Trattatello", Font.BOLD, 30));
-
+		// Creation du score
+		
+		scoreLabel = new JLabel();
+		scoreLabel.setFont(new Font("Trattatello", Font.BOLD, 25));
 		scoreLabel.setAlignmentX(CENTER_ALIGNMENT);
 		scoreLabel.setAlignmentY(CENTER_ALIGNMENT);
 		updateScore();
 		add(scoreLabel);
+		
+		 // Creation des vies
+		
+		JPanel lvPane = new JPanel(new GridLayout(1, 4));
+		lvPane.setBackground(Color.black);
+		JLabel livesPn = new JLabel("<html><font color='WHITE'>LIVES </font></html>");
+		livesPn.setFont(new Font("Trattatello", Font.BOLD, 25));
+		livesPn.setAlignmentX(CENTER_ALIGNMENT);
+		livesPn.setAlignmentY(CENTER_ALIGNMENT);
+		lvPane.add(livesPn);
+		
+		for(int i = 0; i < lives; i++) {
+			ImageIcon icon = new ImageIcon("ressources" + File.separator + "life.png");
+			JLabel thumb = new JLabel();
+			thumb.setBackground(Color.black);
+			thumb.setIcon(icon);
+			
+			lvPane.add(thumb);
+		}
+		
+		add(lvPane);
+		
+		fruitsPane = new JPanel();
+		fruitsPane.setBackground(Color.black);
+		add(fruitsPane);
 	
 	}
-	
+		
 	public void updateScore() {
 		scoreLabel.setText("<html><font color='WHITE'>SCORE </font> <font color = 'YELLOW'>" + score + "</font></html>");
 	}
