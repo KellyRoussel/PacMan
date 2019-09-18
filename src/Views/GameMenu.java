3f5ea73a0e21ee0f433b0bb63bbe0c1f003a425e
Meftahi12 <abdomeftahi7@gmail.com>
2019-09-17 22:40:58 -0400

    resume

--- src/Views/GameMenu.java
+++ src/Views/GameMenu.java
@@ -111,6 +111,7 @@ public class GameMenu extends JPanel implements ActionListener{
 		String cmd = e.getActionCommand();
 		switch(cmd) {
 		case "Start Game":
+			gameController.resume();
 			gameController.startGame();	
 			break;
 		case "Audio":
