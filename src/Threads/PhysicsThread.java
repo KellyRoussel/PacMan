2bdac0adec31fa623898b7da261c2b243bb32079
Vincent Bougie <vince.962@hotmail.com>
2019-09-18 01:07:27 -0400

    Advanced Collision Arc + Rectangle Ghost Implemented
    Advanced Collision Arc + Rectangle Ghost Implemented

--- src/Threads/PhysicsThread.java
+++ src/Threads/PhysicsThread.java
@@ -13,7 +13,7 @@ import Models.Foods.Food;
 public class PhysicsThread extends Thread
 {
 	private volatile boolean isRunning;
-	private final int SLEEP_TIMER = 10; //ms
+	private final int SLEEP_TIMER = 5; //ms
 
 	private final PacMan pacMan;
 	private final List<Ghost> ghost;
@@ -33,48 +33,48 @@ public class PhysicsThread extends Thread
 
 	public synchronized boolean catchCollisionPacManGhost(PacMan var_pacMan, List<Ghost> var_ghost) {
 
+		//Rectangle = Basic Pacman Hitbox
+		//Ellipse = Advanced Pacman Hitbox
 		Rectangle pacmanRectangle = var_pacMan.getRectangle();
 		Ellipse2D.Float pacmanEllipse = var_pacMan.getEllipse();
 
 		for(Ghost x : var_ghost) 
 		{
+			//ghostRectangle = Basic Ghost Hitbox
+			//ghostRectangleLow = Advanced Lower Ghost Hitbox
+			//ghostArcTop = Advanced Upper Ghost Hitbox
 			Rectangle ghostRectangle = x.getRectangle();
 			Rectangle ghostRectangleLow = x.getAdvancedLowerRectangle();
 			Arc2D ghostArcTop = x.getAdvancedTopArc();
 			
 			if(pacmanRectangle.intersects(x.getRectangle())) 
 			{
-				return true;
-				/*if(advancedCatchCollisionTactics(pacmanEllipse, ghostRectangleLow, ghostArcTop))
+				System.out.println("Basic Collision Detected");
+				if(advancedCatchCollisionTactics(pacMan.getEllipse(),x.getAdvancedLowerRectangle(),x.getAdvancedTopArc()))
 				{
 					return true;
-				}*/
+				}
+				System.out.println("Basic Not Valid Collision Detected");
 			}
 		}
 		return false;
 	}
 	
-	private synchronized boolean advancedCatchCollisionTactics(Ellipse2D.Float var_pacman, Rectangle var_ghostbottom,Arc2D ghostArcTop)
+	private synchronized boolean advancedCatchCollisionTactics(Ellipse2D.Float var_pacman, Rectangle var_ghostlower, Arc2D.Float var_ghostUpper)
 	{
-		if(var_pacman.intersects(var_ghostbottom))
+		if(var_pacman.intersects(var_ghostlower))
 		{
+			System.out.println("Advanced Rectangle Collision");
+			return true;
+		}
+		else if(((var_pacman.getWidth()/2)+(var_ghostUpper.getWidth()/2)) >= ((var_pacman.x+(var_pacman.getWidth()/2) - var_ghostUpper.getCenterX())) && ((var_pacman.getHeight()/2)+(var_ghostUpper.getHeight()/2)) >= ((var_pacman.y+(var_pacman.getHeight()/2) - var_ghostUpper.getCenterY())))
+		{
+			System.out.println("Advanced Arc Collision");
 			return true;
 		}
 		return false;
 	}
 	
-	/*public synchronized boolean catchCollisionPacManPacGum(PacMan var_pacMan, List<Food> var_pacgum) {
-
-		Rectangle pacmanRectangle = var_pacMan.getRectangle();
-
-		for(PacGum x : var_pacgum) 
-		{
-			if(pacmanRectangle.intersects(x.getRectangle())) {
-				return true;
-			}
-		}
-		return false;
-	}*/
 
 	public void stopThread() {
 		isRunning = false;
