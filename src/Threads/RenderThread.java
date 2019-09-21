befe73b3eaf7def7e67373320818a92d2ad7e27f
Meftahi12 <abdomeftahi7@gmail.com>
2019-09-21 16:28:09 -0400

    edit

--- src/Threads/RenderThread.java
+++ src/Threads/RenderThread.java
@@ -1,85 +1,169 @@
-package Tests;
+package Threads;
 
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
-
-import java.nio.channels.InterruptedByTimeoutException;
-
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import Controllers.GameController;
-import Threads.RenderThread;
-import Views.MainGame;
+import Models.Maze;
+import Models.Characters.Ghost;
+import Models.Characters.PacMan;
+import Models.Foods.Food;
+import Views.GamePanel;
+import Views.StatusBar;
 
-public class RenderThreadTest {
-		
-		private static final long JOIN_TIMER = 500;
-		private static RenderThread renderThread;
+public class RenderThread extends Thread {
 
-		@BeforeEach
-		public void setUp() throws Exception 
-		{
-			GameController gController = new GameController(new MainGame());
-			gController.startGame();
-			renderThread = gController.gettRender();
-			
-			assertTrue("Thread should be started", renderThread.isAlive());
+	private PacMan pacMan;
+	private GamePanel gamePanel;
+	private Maze maze;
+	private ArrayList<Food> foodList;
+	private ArrayList<Ghost> ghostList;
+	private StatusBar statusBar;
+	private long date = 0;
+	private TimerThread timerThread;
+
+	private static AtomicBoolean running = new AtomicBoolean(false);
+	private static AtomicBoolean resume = new AtomicBoolean(false);
+	private static AtomicBoolean pause = new AtomicBoolean(false);
+
+	public RenderThread(PacMan pacMan, GamePanel gamePanel, Maze maze, ArrayList<Food> foodList,
+			ArrayList<Ghost> ghostList, StatusBar statusBar) {
+		super();
+		this.pacMan = pacMan;
+		this.gamePanel = gamePanel;
+		this.maze = maze;
+		this.foodList = foodList;
+		this.ghostList = ghostList;
+		this.statusBar = statusBar;
+	}
+	// Boucle du jeu
+
+	public boolean isPause() {
+		return pause.get();
+	}
+
+	public void setPause(boolean pause) {
+		this.pause = new AtomicBoolean(pause);
+	}
+
+	public boolean isRunning() {
+		return running.get();
+	}
+
+	public void setRunning(boolean running) {
+		this.running = new AtomicBoolean(running);
+	}
+
+	@Override
+	public void run() {
+		setRunning(true);
+		System.out.println("START - " + this.getName());
+		setPause(true);
+		int counter = 0;
+		while (isRunning()) {
+			System.out.println(isResume());
+			System.out.println(isRunning());
+			try {
+				if (pacMan.isDead()) {
+					pacMan.deadAnimate();
+				}
+				if (isPause()) {
+					pause();
+				} else if (isResume())
+					Resume();
+
+				gamePanel.gameRender(pacMan, maze, foodList, ghostList);
+				gamePanel.paintScreen();
+
+				long currentTime = System.currentTimeMillis();
+				int sleeptime = (int) (1000L / GameController.getFPS());
+				if (date != 0 && (currentTime - date - sleeptime) != 0)
+					GameController.setFPS((int) (1000 / (currentTime - date - sleeptime) / 2));
+
+				date = currentTime;
+
+				if (GameController.getFPS() > 60)
+					GameController.setFPS(60);
+				else if (GameController.getFPS() < 30)
+					GameController.setFPS(30);
+
+				if (counter == 0)
+					statusBar.updateFPS("" + GameController.getFPS());
+				counter++;
+				counter = counter % 10;
+
+				Thread.sleep(1000L / (long) GameController.getFPS());
+
+			} catch (InterruptedException ex) {
+
+			}
 		}
+		System.out.println("STOP - " + this.getName());
+	}
 
-		/**
-		 * DESC: Method called AFTER each single test.
-		 * @throws Exception
-		 */
-		@AfterEach
-		public void tearDown() throws Exception 
-		{
-			// Stop the Counter thread		
-			synchronized(renderThread)
-			{
-				try 
-				{		
-					renderThread.stopThread();
-					renderThread.join(JOIN_TIMER);
-					if (renderThread.isAlive())
-					{
-						renderThread.interrupt();
-						throw new InterruptedByTimeoutException();
+	public boolean isResume() {
+		return resume.get();
+	}
+
+	public void pause() {
+		setPause(true);
+		statusBar.updateState("PAUSED");
+	}
+
+	public void Resume() {
+		setPause(false);
+		statusBar.updateState("RESUME");
+
+		GameController.setRESUME(0);
+		while (GameController.getRESUME() < 3) {
+			GameController.setRESUME(GameController.getRESUME() + 1);
+
+			gamePanel.gameRender(pacMan, maze, foodList, ghostList);
+			gamePanel.paintScreen();
+
+			timerThread = new Threads.TimerThread(1);
+			timerThread.start();
+			timerThread.setName(" RESUME TIMER");
+
+			synchronized (timerThread) {
+
+				try {
+					timerThread.wait(1 * 1000 + 500);
+					timerThread.join(GameController.getJoinTimer());
+					if (timerThread.isAlive()) {
+						timerThread.interrupt();
 					}
-					
-				} 
-				catch (InterruptedException | InterruptedByTimeoutException e) 
-				{
-					assertEquals("Throw exception occurred", null, e);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
-				
-				assertFalse("Thread should be stopped", renderThread.isAlive());
 			}
-		}
-		
-	
-		
-		@Test
-		public void testUpdatedFPS() {
-			synchronized (renderThread) {
-				int fps = renderThread.updatedFPS(30, System.currentTimeMillis());
-				
-				assertTrue(fps <= 60 && fps >= 30);
-			}
-		}
-		
-		@Test
-		public void testResume() {
 
-			synchronized (renderThread) {
-				synchronized (renderThread) {
-					renderThread.setResume(true);
-					assertEquals(renderThread.isResume(), true);
-				}
-			}
 		}
+		statusBar.updateState("PLAY");
+	}
+
+	public void stopThread() {
+
+		setRunning(false);
+		try {
+			this.join(300);
+			if (this.isAlive()) {
+
+				this.interrupt();
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public int updatedFPS(int i, long currentTimeMillis) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setResume(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
